220b4cea14dbbae429bda2b4f8e176d774b12bf1
Andrew <andrew@thinkandrew.(none)>
2011-04-15 23:31:46 -0700

    initial commit

---
+++ src/net/thinkandrew/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.thinkandrew.helloworld;
+
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class HelloWorld extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
